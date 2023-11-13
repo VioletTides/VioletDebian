0f65bd00c6aebf7719d7cf6c32273166721eb35c
VioletTides <ross34anderson@gmail.com>
2023-11-13 18:09:56 -0500

    sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,13 +10,13 @@ ALACRITTY_DIR="$HOME/.config/alacritty"
 VIM_DIR="$HOME"
 RANGER_DIR="$HOME/.config/ranger"
 
-mkdir -p "$CONFIG_DIR"
+sudo mkdir -p "$CONFIG_DIR"
 
 sudo apt-get update
 sudo apt-get install -y build-essential libx11-dev libxinerama-dev libxft-dev libharfbuzz-dev git
 sudo apt-get install -y xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
 
-mkdir -p "$SUCKLESS_DIR"
+sudo mkdir -p "$SUCKLESS_DIR"
 git clone https://git.suckless.org/dwm "$SUCKLESS_DIR/dwm"
 git clone https://git.suckless.org/dmenu "$SUCKLESS_DIR/dmenu"
 
@@ -31,20 +31,20 @@ sudo apt-get install -y vim alacritty ranger zsh firefox-esr pipewire amixer
 chsh -s "$(which zsh)"
 
 # Copy Alacritty config file
-cp "$ROOT_DIR/config/alacritty/alacritty.yml" "$ALACRITTY_DIR/alacritty.yml"
+sudo cp "$ROOT_DIR/config/alacritty/alacritty.yml" "$ALACRITTY_DIR/alacritty.yml"
 
 # Copy Ranger config files
-cp -r "$ROOT_DIR/config/ranger" "$RANGER_DIR"
+sudo cp -r "$ROOT_DIR/config/ranger" "$RANGER_DIR"
 
 # Setup Vim-Plug and install if not already
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
-    mkdir -p "$HOME/.vim/autoload"
+    sudo mkdir -p "$HOME/.vim/autoload"
     curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Copy Vim config file
-cp "$ROOT_DIR/vimrc" "$VIM_DIR/.vimrc"
+sudo cp "$ROOT_DIR/vimrc" "$VIM_DIR/.vimrc"
 
 # Install Vim plugins using Vim-Plug
 vim +PlugInstall +qall
@@ -69,4 +69,4 @@ cd "$SUCKLESS_DIR/dmenu" || exit
 sudo make install clean
 
 echo "Setup Complete!"
-start
+startx
