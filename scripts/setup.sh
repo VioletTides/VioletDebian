7c647cd1f12601881135ee453d351aa468c8c41d
VioletTides <ross34anderson@gmail.com>
2023-11-13 17:56:19 -0500

    fixed wronf filepaths git push

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,31 +3,26 @@
 ROOT_DIR="$HOME/dotfiles"
 CONFIG_DIR="$HOME/.config"
 FONT_DIR="/usr/share/fonts"
-SUCKLESS_DIR="$HOME./suckless"
+SUCKLESS_DIR="$HOME/.suckless"
 XINITRC_DIR="$HOME"
 XSESSIONS_DIR="/usr/share/xsessions/"
-ALACRITTY_DIR="$HOME./config/alacritty"
+ALACRITTY_DIR="$HOME/.config/alacritty"
 VIM_DIR="$HOME"
-RANGER_DIR="$HOME./config/ranger"
+RANGER_DIR="$HOME/.config/ranger"
 
-mkdir "$CONFIG_DIR"
+mkdir -p "$CONFIG_DIR"
 
 sudo apt-get update
-# Install dependencies
-sudo apt install -y build-essential libx11-dev libxinerama-dev libxft-dev libharfbuzz-dev 
-sudo apt install -y xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
+sudo apt-get install -y build-essential libx11-dev libxinerama-dev libxft-dev libharfbuzz-dev git
+sudo apt-get install -y xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
 
-mkdir "$SUCKLESS_DIR"
+mkdir -p "$SUCKLESS_DIR"
 git clone https://git.suckless.org/dwm "$SUCKLESS_DIR/dwm"
 git clone https://git.suckless.org/dmenu "$SUCKLESS_DIR/dmenu"
-cd "$SUCKLESS_DIR/dwm" || exit
-sudo make install clean
-cd "$SUCKLESS_DIR/dmenu" || exit
-sudo make install clean
 
 echo "exec dwm" >> "$XINITRC_DIR/.xinitrc"
 
-cp "$ROOT_DIR/config/dwm/dwm.desktop" "usr/share/xsessions"
+sudo cp "$ROOT_DIR/config/dwm/dwm.desktop" "/usr/share/xsessions"
 
 # Install Vim, Alacritty, Ranger, zsh, etc
 sudo apt-get install -y vim alacritty ranger zsh firefox-esr pipewire amixer
@@ -36,7 +31,10 @@ sudo apt-get install -y vim alacritty ranger zsh firefox-esr pipewire amixer
 chsh -s "$(which zsh)"
 
 # Copy Alacritty config file
-cp "$ROOT_DIR/config/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+cp "$ROOT_DIR/config/alacritty/alacritty.yml" "$ALACRITTY_DIR/alacritty.yml"
+
+# Copy Ranger config files
+cp -r "$ROOT_DIR/config/ranger" "$RANGER_DIR"
 
 # Setup Vim-Plug and install if not already
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
@@ -46,19 +44,17 @@ if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
 fi
 
 # Copy Vim config file
-cp "$ROOT_DIR/vimrc" "$HOME/.vimrc"
+cp "$ROOT_DIR/vimrc" "$VIM_DIR/.vimrc"
 
 # Install Vim plugins using Vim-Plug
 vim +PlugInstall +qall
 
-
-
 # INSTALL FONTS
 # Create a fonts directory if it doesn't exist
 if [ -d "$FONT_DIR" ]; then
-	echo "Font directory already exists, continuing..."
+    echo "Font directory already exists, continuing..."
 else
-	sudo mkdir -p "$FONT_DIR"
+    sudo mkdir -p "$FONT_DIR"
 fi
 
 # Copy Mononoki font files into the font directory
@@ -67,5 +63,10 @@ sudo cp "$ROOT_DIR/fonts/"* "$FONT_DIR/"
 # Update the font cache
 sudo fc-cache -f -v
 
+cd "$SUCKLESS_DIR/dwm" || exit
+sudo make install clean
+cd "$SUCKLESS_DIR/dmenu" || exit
+sudo make install clean
+
 echo "Setup Complete!"
-startx
+start
