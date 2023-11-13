99df3e1b31a71b19fae79591683cf55f28590520
VioletTides <ross34anderson@gmail.com>
2023-11-13 17:38:14 -0500

    added X dependencies and firefox

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ROOT_DIR="$HOME/dotfiles" # This just goes up one level to the "dotfiles" repo root
+ROOT_DIR="$HOME/dotfiles"
+CONFIG_DIR="$HOME/.config"
 FONT_DIR="/usr/share/fonts"
 SUCKLESS_DIR="$HOME./suckless"
 XINITRC_DIR="$HOME"
@@ -9,29 +10,14 @@ ALACRITTY_DIR="$HOME./config/alacritty"
 VIM_DIR="$HOME"
 RANGER_DIR="$HOME./config/ranger"
 
-DIRECTORIES=("$ROOT_DIR" "$FONT_DIR" "$SUCKLESS_DIR" "$XINITRC_DIR" "$XSESSIONS_DIR" "$ALACRITTY_DIR" "$VIM_DIR" "$RANGER_DIR")
-DIRECTORIES_STR=("root" "fonts" "suckless" "xinitrc" "xsessions" "alacritty config" "vim config" "ranger config")
-USER_DIRECTORIES=()
-
-# COPY CONFIGS INTO DEFAULT LOCATIONS
-for ((i=0; i<${#DIRECTORIES[@]}; i++)); do
-    dir="${DIRECTORIES[$i]}"
-    dir_str="${DIRECTORIES_STR[$i]}"
-    
-    read -p "Enter the path to your $dir_str (or press enter to use the default \"$dir\"): " directory_input
-    directory_input=$(echo "$directory_input" | tr -d '[:space:]')
-
-    # Use the default value if the user input is empty
-    USER_DIRECTORIES+=("${directory_input:-$dir}")
-done
-
-DIRECTORIES=("${USER_DIRECTORIES[@]}")
+mkdir "$CONFIG_DIR"
 
 sudo apt-get update
-# Install dependencies for dwm
-sudo apt install -y build-essential libx11-dev libxinerama-dev libxft-dev libharfbuzz-dev
+# Install dependencies
+sudo apt install -y build-essential libx11-dev libxinerama-dev libxft-dev libharfbuzz-dev 
+sudo apt install -y xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
 
-mkdir -p "$SUCKLESS_DIR"
+mkdir "$SUCKLESS_DIR"
 git clone https://git.suckless.org/dwm "$SUCKLESS_DIR/dwm"
 git clone https://git.suckless.org/dmenu "$SUCKLESS_DIR/dmenu"
 cd "$SUCKLESS_DIR/dwm" || exit
@@ -39,18 +25,18 @@ sudo make install clean
 cd "$SUCKLESS_DIR/dmenu" || exit
 sudo make install clean
 
-echo "exec dwm" >> "$XINITRC_DIR"
+echo "exec dwm" >> "$XINITRC_DIR/.xinitrc"
 
 cp "$ROOT_DIR/config/dwm/dwm.desktop" "usr/share/xsessions"
 
-# Install Vim, Alacritty, Ranger, and zsh
-sudo apt-get install -y vim alacritty ranger zsh
+# Install Vim, Alacritty, Ranger, zsh, etc
+sudo apt-get install -y vim alacritty ranger zsh firefox-esr pipewire amixer
 
 # Set Zsh as the default shell
 chsh -s "$(which zsh)"
 
 # Copy Alacritty config file
-cp "$ROOT_DIR/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+cp "$ROOT_DIR/config/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 
 # Setup Vim-Plug and install if not already
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
@@ -82,3 +68,4 @@ sudo cp "$ROOT_DIR/fonts/"* "$FONT_DIR/"
 sudo fc-cache -f -v
 
 echo "Setup Complete!"
+startx
