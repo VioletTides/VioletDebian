06c45442a6991603009cdd7fb2fd343195c4fc3d
anderr26 <ross34anderson@gmail.com>
2023-11-15 16:30:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,31 +43,31 @@ install_suckless() {
     apt-get update
     apt-get install -y make build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
 
-    mkdir -p "$SUCKLESS_DIR"
+    mkdir -p "$SUCKLESS_DIR" || { echo "Failed to make the suckless directory"; exit 1; }
     cd "$SUCKLESS_DIR" || exit
     git clone https://git.suckless.org/dwm
     git clone https://git.suckless.org/dmenu
 
     # Build and install dwm
-    cd dwm || exit
+    cd /$SUCKLESS_DIR/dwm || || { echo "Failed to cd into dwm"; exit 1; }
     make clean install || { echo "Failed to build and install dwm"; exit 1; }
     # Build and install dmenu
-    cd ../dmenu || exit
+    cd /$SUCKLESS_DIR/dmenu || { echo "Failed to cd into dmenu"; exit 1; }
     make clean install || { echo "Failed to build and install dmenu"; exit 1; }
 
     # Copy the config.h files to the dwm and dmenu directories
-    cp "$REPO_DIR/config/suckless/dwm/config.h" "$SUCKLESS_DIR/dwm/config.h"
-    cp "$REPO_DIR/config/suckless/dmenu/config.h" "$SUCKLESS_DIR/dmenu/config.h"
+    cp "$REPO_DIR/config/suckless/dwm/config.h" "$SUCKLESS_DIR/dwm/config.h" || { echo "Failed to copy dwm config.h"; exit 1; }
+    cp "$REPO_DIR/config/suckless/dmenu/config.h" "$SUCKLESS_DIR/dmenu/config.h" || { echo "Failed to copy dmenu config.h"; exit 1; }
 
     # Install dependencies for xorg
     apt-get install -y xorg xserver-xorg xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
 
-    mkdir -p "$XINITRC_DIR"
-    mkdir -p "$XSESSIONS_DIR"
+    mkdir -p "$XINITRC_DIR" || { echo "Failed to make the .xinitrc directory"; exit 1; }
+    mkdir -p "$XSESSIONS_DIR" || { echo "Failed to make the .desktop directory"; exit 1; }
     # Create and init the .xinitrc file in the specified directory
-    echo "exec dwm" >> "$XINITRC_DIR/.xinitrc"
+    echo "exec dwm" >> "$XINITRC_DIR/.xinitrc" || { echo "Failed to create and init the .xinitrc file"; exit 1; }
     # Copy the .desktop file to the specified directory
-    cp "$REPO_DIR/config/suckless/dwm/dwm.desktop" "$XSESSIONS_DIR"
+    cp "$REPO_DIR/config/suckless/dwm/dwm.desktop" "$XSESSIONS_DIR" || { echo "Failed to copy the .desktop file"; exit 1; }
 }
 
 install_kitty() {
@@ -120,8 +120,8 @@ install_fonts() {
 }
 
 
-#install_suckless
-install_kitty
+install_suckless
+#install_kitty
 #install_ranger
 #install_vim
 #install_fonts
