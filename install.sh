20386fdbc1754b31738f98da3c39be5ac5cf21d7
anderr26 <ross34anderson@gmail.com>
2023-11-15 17:36:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Check if Script is Run as Root
 if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
+    echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+    exit 1
 fi
 
 username=$(id -u -n 1000)
@@ -25,7 +25,7 @@ RANGER_DIR="/home/$username/.config/ranger" # The directory where the Ranger con
 mkdir -p $CONFIG_DIR
 mkdir -p $FONT_DIR
 mkdir -p $RANGER_DIR
-mkdir -p /home/$username/Pictures   
+mkdir -p /home/$username/Pictures
 mkdir -p /home/$username/Pictures/backgrounds
 
 # Install basics
@@ -39,28 +39,28 @@ install_suckless() {
     # Install dependencies for dwm and dmenu
     apt-get update
     apt-get install -y make build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
-
+    
     mkdir -p "$SUCKLESS_DIR" || { echo "Failed to make the suckless directory"; exit 1; }
     cd "$SUCKLESS_DIR" || exit
     git clone https://git.suckless.org/dwm
     git clone https://git.suckless.org/dmenu
-
+    
     # Build and install dwm
     cd /$SUCKLESS_DIR/dwm || { echo "Failed to cd into dwm"; exit 1; }
     make clean install || { echo "Failed to build and install dwm"; exit 1; }
     # Build and install dmenu
     cd /$SUCKLESS_DIR/dmenu || { echo "Failed to cd into dmenu"; exit 1; }
     make clean install || { echo "Failed to build and install dmenu"; exit 1; }
-
+    
     # Copy the config.h files to the dwm and dmenu directories
     cp "$REPO_DIR/config/suckless/dwm/config.h" "$SUCKLESS_DIR/dwm/config.h" || { echo "Failed to copy dwm config.h"; exit 1; }
     #cp "$REPO_DIR/config/suckless/dmenu/config.h" "$SUCKLESS_DIR/dmenu/config.h" || { echo "Failed to copy dmenu config.h"; exit 1; }
-
+    
     # Install dependencies for xorg
-    apt-get install -y xorg xserver-xorg xserver-xorg-core xserver-xorg-video-intel xorg-xinit x11-xserver-utils
-
+    apt-get install -y xorg xserver-xorg xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
+    
     mkdir -p "$XSESSIONS_DIR" || { echo "Failed to make the .desktop directory"; exit 1; }
-
+    
     # Create and init the .xinitrc file in the specified directory
     echo "exec dwm" >> "$XINITRC_DIR/.xinitrc" || { echo "Failed to create and init the .xinitrc file"; exit 1; }
     # Create and init the .xsessions file in the specified directory
@@ -70,7 +70,28 @@ install_suckless() {
 }
 
 install_kitty() {
-    bash scripts/kitty.sh
+    # Check if kitty is already installed
+    if command -v kitty &>/dev/null; then
+        echo "Kitty is already installed."
+        exit 0
+    fi
+    
+    # Install kitty
+    curl -L https://sw.kovidgoyal.net/kitty/aptk.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kovidgoyal-archive-keyring.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/kovidgoyal-archive-keyring.gpg] https://sw.kovidgoyal.net/kitty/nightly/apt buster main' | tee /etc/apt/sources.list.d/kitty.list
+    apt update
+    apt install kitty -y
+    
+    # Set kitty as default terminal
+    update-alternatives --set x-terminal-emulator "$(which kitty)"
+    
+    # Remove the repo after installation
+    rm -rf /kitty
+    
+    # Print installation status and version information to terminal
+    echo "Kitty Installation Complete"
+    echo -e "\nKitty Version:"
+    kitty --version
 }
 
 ### INSTALL AND CONFIGURE RANGER ###
@@ -82,22 +103,22 @@ install_ranger() {
 }
 
 ### INSTALL AND CONFIGURE VIM ###
-install_vim() { 
+install_vim() {
     # Install Vim
     apt install vim -y
-
+    
     # Setup Vim-Plug and install if not already
     if [ -f "$VIM_DIR/.vim/autoload/plug.vim" ]; then
         echo "Vim-Plug already installed, continuing..."
     else
         echo "Installing Vim-Plug..."
         curl -fLo "$VIM_DIR/.vim/autoload/plug.vim" --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
-
+    
     # Configure Vim
     sudo cp "$REPO_DIR/config/vim/.vimrc" "$VIM_DIR/.vimrc"
-
+    
     # Install Vim plugins using Vim-Plug
     vim +PlugInstall +qall
 }
@@ -106,14 +127,14 @@ install_vim() {
 install_fonts() {
     # Go into the repo directory
     cd $REPO_DIR
-
+    
     # Install the Mononoki font
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Mononoki.zip
     unzip Mononoki.zip -d $FONT_DIR
-
+    
     # Update the font cache
     fc-cache -f -v
-
+    
     # Clean up the Mononoki zip from the current directory
     rm ./Mononoki.zip
 }
@@ -122,7 +143,7 @@ install_fonts() {
 install_suckless
 install_kitty
 #install_ranger
-#install_vim
+install_vim
 #install_fonts
 chown -R $username:$username /home/$username
 chown $username:$username $FONTDIR*
