6be467c5999db505f82b99b53e6149d34ec4dcda
anderr26 <ross34anderson@gmail.com>
2023-11-15 15:21:20 -0500

    testing integrating kitty.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apt upgrade -y
 cd $REPO_DIR
 
 CONFIG_DIR="$HOME/.config" # The directory where config files are stored
-FONT_DIR="/usr/share/fonts" # The directory where fonts are stored
+FONT_DIR="$HOME/$username/fonts" # The directory where fonts are stored
 SUCKLESS_DIR="$HOME/.suckless" # The directory where suckless programs are stored
 XINITRC_DIR="$HOME" # The directory where the .xinitrc file is stored
 XSESSIONS_DIR="/usr/share/xsessions/" # The directory where the .desktop files are stored
@@ -24,16 +24,17 @@ ALACRITTY_DIR="$HOME/.config/alacritty" # The directory where the Alacritty conf
 VIM_DIR="$HOME" # The directory where the Vim config file is stored
 RANGER_DIR="$HOME/.config/ranger" # The directory where the Ranger config files are stored
 
+
 # Install basics
-sudo apt-get install -y curl zsh wget firefox-esr amixer pulseaudio
+apt-get install -y curl zsh wget firefox-esr pulseaudio
 
 # Set Zsh as the default shell
 chsh -s "$(which zsh)"
 
 install_suckless() {
     # Install dependencies for dwm and dmenu
-    sudo apt-get update
-    sudo apt-get install -y make build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
+    apt-get update
+    apt-get install -y make build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
 
     mkdir -p "$SUCKLESS_DIR"
     cd "$SUCKLESS_DIR" || exit
@@ -42,54 +43,28 @@ install_suckless() {
 
     # Build and install dwm
     cd dwm || exit
-    sudo make clean install
+    make clean install
     # Build and install dmenu
     cd ../dmenu || exit
-    sudo make clean install
+    make clean install
 
     # Copy the config.h files to the dwm and dmenu directories
-    sudo cp "$REPO_DIR/config/suckless/dwm/config.h" "$SUCKLESS_DIR/dwm/config.h"
-    sudo cp "$REPO_DIR/config/suckless/dmenu/config.h" "$SUCKLESS_DIR/dmenu/config.h"
+    cp "$REPO_DIR/config/suckless/dwm/config.h" "$SUCKLESS_DIR/dwm/config.h"
+    cp "$REPO_DIR/config/suckless/dmenu/config.h" "$SUCKLESS_DIR/dmenu/config.h"
 
     # Install dependencies for xorg
-    sudo apt-get install -y xorg xserver-xorg xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
+    apt-get install -y xorg xserver-xorg xserver-xorg-core xserver-xorg-video-intel xinit x11-xserver-utils
 
     mkdir -p "$XINITRC_DIR"
     mkdir -p "$XSESSIONS_DIR"
     # Create and init the .xinitrc file in the specified directory
     echo "exec dwm" >> "$XINITRC_DIR/.xinitrc"
     # Copy the .desktop file to the specified directory
-    sudo cp "$REPO_DIR/config/suckless/dwm/dwm.desktop" "$XSESSIONS_DIR"
+    cp "$REPO_DIR/config/suckless/dwm/dwm.desktop" "$XSESSIONS_DIR"
 }
 
-install_alacritty() {
-    # Install Alacritty dependencies
-    sudo apt-get install -y cmake rust cargo pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
-
-    # Clone Alacritty repository
-    cd "$REPO_DIR" || exit
-    git clone https://github.com/alacritty/alacritty.git
-
-    # Build and install Alacritty
-    cd alacritty || exit
-    
-    # Build Alacritty
-    cargo build --release
-
-    # Install Alacritty (you may need to adjust the path if desired)
-    sudo cp "target/release/alacritty" "/usr/local/bin"
-
-    # Remove Alacritty repository
-    cd "$REPO_DIR" || exit
-    rm -rf alacritty
-
-    # Configure alacritty
-    sudo mkdir -p "$ALACRITTY_DIR"
-    sudo cp "$REPO_DIR/config/alacritty/alacritty.yml" "$ALACRITTY_DIR/alacritty.yml"
-
-    # Set Alacritty as the default terminal
-    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
-    sudo update-alternatives --config x-terminal-emulator
+install_kitty() {
+    bash scripts/kitty.sh
 }
 
 ### INSTALL AND CONFIGURE RANGER ###
@@ -120,25 +95,35 @@ install_vim() {
 
 ### INSTALL AND CONFIGURE FONTS ###
 install_fonts() {
+    # Go into the repo directory
+    cd $REPO_DIR
+
     # Create a fonts directory if it doesn't exist
     if [ -d "$FONT_DIR" ]; then
         echo "Font directory already exists, continuing..."
     else
-        sudo mkdir -p "$FONT_DIR"
+        mkdir -p "$FONT_DIR"
     fi
 
-    # Copy Mononoki font files into the font directory
-    sudo cp "$REPO_DIR/fonts/" "$FONT_DIR/"
+    # Install the Mononoki font
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Mononoki.zip
+    unzip Mononoki.zip -d $FONT_DIR
 
     # Update the font cache
-    sudo fc-cache -f -v
+    fc-cache -f -v
+
+    # Clean up the Mononoki zip from the current directory
+    rm ./Mononoki.zip
 }
 
-install_suckless
-install_alacritty
-install_ranger
-install_vim
 install_fonts
+#install_suckless
+install_kitty
+#install_ranger
+#install_vim
+
+chown -R $username:$username /$HOME/$username
+chown $username:$username /home/$username/.fonts/*
 
 echo "Setup Complete!"
 startx
