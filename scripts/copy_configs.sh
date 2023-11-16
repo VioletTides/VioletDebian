c88fbae2cba6bdff2b7c9cf3a753031c8029affe
VioletTides <ross34anderson@gmail.com>
2023-11-16 15:34:39 -0500

    updated copy_configs.sh

--- scripts/copy_configs.sh
+++ scripts/copy_configs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cp -r "$HOME/.suckless/dwm/config.h" "$HOME/dotfiles/config/suckless/dwm/config.h"
-cp -r "$HOME/.vimrc" "$HOME/dotfiles/config/vim/.vimrc"
-cp -r "$HOME/.config/alacritty/alacritty.yml" "$HOME/dotfiles/config/alacritty/alacritty.yml"
+cp -r "$HOME/.suckless/dwm/config.h" "$HOME/VioletDebian/config/suckless/dwm/config.h"
+cp -r "$HOME/.vimrc" "$HOME/VioletDebian/config/vim/.vimrc"
